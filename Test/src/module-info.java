7f427926f5f64f88f111c108b964b98548e4ea64
MounikaPerusomula <168636997+MounikaPerusomula@users.noreply.github.com>
2024-05-08 11:16:31 +0530

    Add files via upload

---
+++ Test/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Suvarchala
+ *
+ */
+module Test {
+}
---
+++ Test/src/test/SampleProgram.java
@@ -0,0 +1,11 @@
+package test;
+
+public class SampleProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to GITHUB");
+
+	}
+
+}
